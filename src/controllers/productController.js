6bbb6cc27d174c6b61bbc9f906a67d015a372d48
Agustin Faggiano <agustinfaggiano>
2021-07-27 01:26:57 -0300

    Actualizacion de Router Products con todas las rutas necesarias. Agregado del Sector Data con archivos Json

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -11,8 +11,20 @@ const controladorProducto = {
             res.render("products/new-item");
         },
 
+        almacenarNuevoProducto: (req, res) => {
+            res.render("products/all-items"); //Provisorio. Luego debe almacenar en Json el nuevo producto
+        },
+
         editarProducto: (req, res) => {
             res.render("products/edit-item");
+        },
+
+        almacenarProductoEditado: (req, res) => {
+            res.render("products/new-item"); //Provisorio. Luego debe almacenar en Json el producto editado (Actualizar BD)
+        },
+        
+        eliminarProducto: (req, res) => {
+            res.render("products/all-items"); //Provisorio. Luego debe borrar de la BD (Json) el producto recibido por ID.
         }
 }
 
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,14 +3,21 @@ const controladorProducto = require ("../controllers/productController");
 const express = require ("express");
 const productRouter = express.Router();
 
-productRouter.get("/", controladorProducto.listadoProductos);
+productRouter.get("/", controladorProducto.listadoProductos); // Listado de Productos
 
-productRouter.get("/detalle", controladorProducto.detalleProducto);
 
-productRouter.get("/nuevoProducto", controladorProducto.productoNuevo);
+productRouter.get("/:id", controladorProducto.detalleProducto); // Detalle de un producto particular
 
-productRouter.get("/edicionProducto", controladorProducto.editarProducto);
 
+productRouter.get("/create", controladorProducto.productoNuevo); // Formulario de Creacion de Producto
+productRouter.post("/", controladorProducto.almacenarNuevoProducto); // Almacenamiento del nuevo producto
+
+
+productRouter.get("/:id/edit", controladorProducto.editarProducto); // Formulario Edición del Producto
+productRouter.put("/:id", controladorProducto.almacenarProductoEditado); // Guardar Producto Editado
+
+
+productRouter.delete("/:id", controladorProducto.eliminarProducto); // Eliminar un Producto 
 
 
 
