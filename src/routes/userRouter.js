1c225880057c6a8be57b7792fe784a480cd5df0c
Agustin Faggiano <agustinfaggiano>
2021-07-31 20:21:32 -0300

    Implementación Funcionalidad de Registro de Usuarios

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,46 @@
-const controladorUsers = require ("../controllers/UserController");
-
+// ************ Require's ************
 const express = require ("express");
 const usersRouter = express.Router();
+const path = require('path');
+const multer = require('multer');
 
+// ************ Controller Require ************
+const controladorUsers = require ("../controllers/UserController");
+
+
+//***  Multer configuration  ****/
+
+const configuracionImagen = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/images/users'));    // Ruta donde almacenamos la foto de perfil
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName =  Date.now() + file.originalname ;   // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+});
+
+const uploadFile = multer({ storage: configuracionImagen });
+
+
+/* Rutas */
+
+/* Inicio de Sesion */
 usersRouter.get("/", controladorUsers.login);
 
+/* Vista Perfil de Usuario */
 usersRouter.get("/profile", controladorUsers.perfil);
 
+/* Editar Perfil de Usuario */
+//crear todo...
+
+
+/* Registración */
 usersRouter.get("/register", controladorUsers.registro);
+usersRouter.post("/register", uploadFile.single('avatar'), controladorUsers.crearNuevoUsuario);
+
+usersRouter.get("/registracionOK", controladorUsers.registracionExitosa);
+
 
 
 
