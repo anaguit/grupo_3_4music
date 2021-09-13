da5cac738b84f5eaca910b8bffde86cb5639b938
Estefi Colodro <estefi.colodro@gmail.com>
2021-09-13 11:36:35 -0300

    Implementación BD en Controlador-Usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,8 +5,20 @@ const {validationResult} = require("express-validator");
 const bcryptjs = require("bcryptjs")
 /* Lectura de Usuarios del Json */
 const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
-const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+//const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
+const db = require ("../../database/models");
+const { sequelize, Sequelize } = require('../../database/models');
+
+//const { where } = require('sequelize/types');
+const op = Sequelize.Op;
+
+let usuarios;
+db.Usuario.findAll()
+.then((resultados) => {
+     usuarios = resultados;
+    
+});
 
 const controladorUsers = {
         login: (req, res) => {
@@ -20,11 +32,11 @@ const controladorUsers = {
                 let emailBuscar = req.body.email;
                 let passwordIngresada = req.body.contraseña;
                 let usuarioEncontrado;
-                let emailEncontrado
+                let emailEncontrado;
                 for (let i of usuarios){
                     if (emailBuscar == i.email) {
                         emailEncontrado = 1;
-                        if(bcryptjs.compareSync(passwordIngresada, i.password)){
+                        if(bcryptjs.compareSync(passwordIngresada, i.clave)){
                             usuarioEncontrado = i; //usuario encontrado en el JSON
                             break;
                         }
@@ -99,7 +111,7 @@ const controladorUsers = {
             
             if(errors.isEmpty()){ 
             
-            idNuevo=0;
+            /*idNuevo=0;
 
             for (let i of usuarios){
                 if (idNuevo<i.id){
@@ -107,30 +119,31 @@ const controladorUsers = {
                 }
             }
 
-            idNuevo++;
+            idNuevo++;*/
 
-            let nombreImagen = req.file.filename;
-            let compradorSitio = false; // por  defecto es vendedor
+            //let nombreImagen = req.file.filename;
+            let compradorSitio = "vendedor"; // por  defecto es vendedor
             let passEncriptada = bcryptjs.hashSync(req.body.contraseña, 10);
 
             if(req.body.tipoUsuario == 1){
-                compradorSitio = true; // si el valor es 1, se lo registra como comprador
+                compradorSitio = "comprador"; // si el valor es 1, se lo registra como comprador
             }
 
             let usuarioNuevo =  {
-                id:   idNuevo,
+                //id:   idNuevo,
                 nombre: req.body.nombre ,    
                 apellido: req.body.apellido ,
                 email: req.body.email ,
-                password: passEncriptada,
+                clave: passEncriptada,
                 telefono: req.body.telefono,
-                fotoPerfil: nombreImagen,
-                comprador: compradorSitio
+                foto_perfil: "foto.jpg",
+                rol: compradorSitio
             };
 
             usuarios.push(usuarioNuevo);
 
-            fs.writeFileSync(usersFilePath, JSON.stringify(usuarios,null,' '));
+            //fs.writeFileSync(usersFilePath, JSON.stringify(usuarios,null,' '));
+            db.Usuario.create(usuarioNuevo);
 
             res.redirect("/users/registracionOK"); 
         } 
--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -25,7 +25,7 @@ const validatedRegister = [
     body("email").notEmpty().withMessage("Debes escribir un correo electrónico").bail().isEmail().withMessage("Debes escribir un email válido"),
     body("telefono").notEmpty().withMessage("Ingresa un número de telefono").bail().isNumeric().withMessage("Ingresa un número de telefono válido"),
     body("contraseña").notEmpty().withMessage("Debes completar el campo de contraseña"),
-    body("avatar").custom((value, { req }) => {
+    body("avatar").optional().custom((value, { req }) => {
         let file = req.file;
         if (!file) {
             throw new Error("Tienes que subir una imagen")
