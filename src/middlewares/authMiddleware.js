33c68cc6a1df6d1b5414af63c6d1fa25993d37d2
Agustin Faggiano <agustinfaggiano>
2021-09-14 17:33:07 -0300

    Modificacion authMiddleware para que verifique que el ID de la URL coincida con el del User Logueado

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,6 +2,12 @@ function authMiddleware (req, res, next) {
     if(!req.session.usuarioLogueado){
         return res.redirect("/users"); //si no está Logueado, lo redirijo a la Vista de Inicio de Sesion
     }
+    else{
+        if(req.params.id && req.params.id != req.session.usuarioLogueado.id){
+            return res.redirect("/users/profile"); 
+            // si no coincide el ID recibido en la URL con el ID del Usuario Logueado, lo redirecciono al Perfil del User Logueado
+        }
+    }
     next();
 }
 
