3e69c666bd8da94244a8d517b33fb2414a50a0db
Julipoli99 <Polimenijulian9@gmail.com>
2021-09-20 15:30:55 -0300

    .

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,10 @@
 module.exports =
 {
   "development": {
-    "username": "anitaguit",
-    "password": "anitaguit",
-    "database": "anitaguit_4music",
-    "host": "mysql-anitaguit.alwaysdata.net",
+    "username": "root",
+    "password": null,
+    "database": "4music",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3306
   },
@@ -16,11 +16,11 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "anitaguit",
+    "password": "anitaguit",
+    "database": "anitaguit_4music",
+    "host": "mysql-anitaguit.alwaysdata.net",
     "dialect": "mysql",
     "port": 3306
-  }
+  },
 }
