25813e7dd3d95d0e5043a30d3cba5b9d6d99a15a
Julipoli99 <Polimenijulian9@gmail.com>
2021-09-20 15:24:29 -0300

    Conexion de base de datos local con remota

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,12 @@
 module.exports =
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "4music",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "anitaguit",
+    "password": "anitaguit",
+    "database": "anitaguit_4music",
+    "host": "mysql-anitaguit.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
   },
   "test": {
     "username": "root",
@@ -19,6 +20,7 @@ module.exports =
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 3306
   }
 }
